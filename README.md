d76233ec96d11afe23796299778d602dd7fb3c6d
Felipe O. Dominicheli <95759245+Dominicheli@users.noreply.github.com>
2025-10-30 14:56:50 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 O meu projeto treinando e aprendendo mais sobre programação com Filipe Deschamps no curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
 function Home() {
-  return <h1>Oi meu amor, você é a pessoa que me faz feliz dia e noite e quero você pra sempre na minha vida! Te amo!</h1>
+  return (
+    <h1>
+      Oi meu amor, você é a pessoa que me faz feliz dia e noite e quero você pra
+      sempre na minha vida! Te amo!
+    </h1>
+  );
 }
+
 export default Home;
